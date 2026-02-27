448b484dc84d0e54974db306be239d50f55e1be5
F1ejm <karolosrodka@gmail.com>
2026-02-27 22:59:55 +0100

    background

--- background/back/background.gd
+++ background/back/background.gd
@@ -5,19 +5,14 @@ extends TileMap
 @export var player : CharacterBody2D
 
 @export var hight:=  10
-@export var width:= 10
+@export var width:= 30
+
+func _process(delta: float) -> void:
+	for h in hight:
+		for w in width:
+			tile.set_cell(0,Vector2i(w,player.position.y+h),0,Vector2(1,1),0)
+	
 
 func _ready() -> void:
 	global_position = Vector2(0,0)
-
-
-func detector_entered() -> void:
-	tile.set_cell(0,Vector2i(0,0),0,Vector2(1,1),0)
 	
-	for h in hight:
-		for w in width:
-			tile.set_cell(0,Vector2i(w,h),0,Vector2(1,1),0)
-	
-	detector.queue_free()
-			
-			 
