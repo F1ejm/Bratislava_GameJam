1af71fa750e5527a0d6180dc1592b214991b946c
KamilZago <kamil.zagorny1802@gmail.com>
2026-02-28 00:45:11 +0100

    better tail

--- Miner/miner.gd
+++ Miner/miner.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 const minSPEED = 70.0
 const maxSPEED = 700.0
-var currentSPEED = 200
+var currentSPEED = 200.0
+const maxhistory = 1000
 
 var torotation = 0
 
@@ -10,15 +11,17 @@ var torotation = 0
 
 func _ready() -> void:
 	for i in range(0,10):
-		spawnTail()
+		spawnTail(1)
 
 func _physics_process(delta: float) -> void:
 	
 	var direction := (get_global_mouse_position() - self.global_position).normalized() * 100
 	if direction.y < 0:
 		direction.y = 0
+		(direction * 1000).normalized()
 		
 	direction.y += 35
+	print(direction)
 	
 	currentSPEED += (0.35 - (abs(self.rotation - (atan2(direction.y, direction.x)- PI/2)))) * delta * 50
 	
@@ -29,22 +32,27 @@ func _physics_process(delta: float) -> void:
 	
 	torotation = atan2(direction.y, direction.x) - PI/2
 	
-	#print(currentSPEED)
-	
 	self.rotation= rotate_toward(self.rotation,torotation,delta *3)
 	
-	player_camera.position.x = lerp(player_camera.position.x,self.position.x, 0.02 - ((currentSPEED / maxSPEED) * 0.01))
+	player_camera.position.x = lerp(player_camera.position.x,self.position.x, 0.02 - ((currentSPEED / maxSPEED) * 0.005))
 	
-	player_camera.position.y = lerp(player_camera.position.y,self.position.y, 0.02 + ((currentSPEED / maxSPEED) * 0.05))
+	player_camera.position.y = lerp(player_camera.position.y,self.position.y, 0.04 + ((currentSPEED / maxSPEED) * 0.05))
 	
 	velocity = Vector2.RIGHT.rotated(self.rotation + PI/2) * currentSPEED
+	
 	$TailSP.global_rotation = 0
+	
+	Global.history.push_front(self.global_rotation)
+	if(Global.history.size() > maxhistory):
+		Global.history.pop_back()
+	
 	move_and_slide()
 	
-func spawnTail():
+func spawnTail(number: int):
 	if($TailSP.get_children().is_empty()):
 		var scene := preload("res://Miner/tail.tscn")
 		var tail := scene.instantiate()
+		tail.number = number
 		$TailSP.add_child(tail)
 	else:
-		$TailSP.get_child(0).spawnTail()
+		$TailSP.get_child(0).spawnTail(number+1)
--- Miner/tail.gd
+++ Miner/tail.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var number: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,13 +8,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	global_rotation = lerp(global_rotation,get_parent().get_parent().global_rotation,6*delta)
+	if(Global.history.size() >= number*12):
+		global_rotation = Global.history[number*12 -1]
 	$TailSP.global_rotation = 0
 	
-func spawnTail():
+	
+func spawnTail(number: int):
 	if($TailSP.get_children().is_empty()):
 		var scene := preload("res://Miner/tail.tscn")
 		var tail := scene.instantiate()
+		tail.number = number
 		$TailSP.add_child(tail)
 	else:
-		$TailSP.get_child(0).spawnTail()
+		$TailSP.get_child(0).spawnTail(number+1)
