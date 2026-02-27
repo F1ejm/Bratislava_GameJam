cfd3733240cfe2b2c6e6333f87ff50451fcc1bb7
F1ejm <karolosrodka@gmail.com>
2026-02-28 00:17:09 +0100

    poasdifosad

--- background/back/background.gd
+++ background/back/background.gd
@@ -1,18 +1,28 @@
 extends TileMap
 
-@export var detector: VisibleOnScreenNotifier2D
 @export var tile: TileMap
 @export var player : CharacterBody2D
 
 @export var hight:=  10
 @export var width:= 30
 
+var _in = true
+
 func _process(delta: float) -> void:
-	for h in hight:
-		for w in width:
-			tile.set_cell(0,Vector2i(w,player.position.y+h),0,Vector2(1,1),0)
-	
+	pass
+	#for h in hight:
+		#for w in width:
+			#tile.set_cell(0,Vector2i(w,player.position.y+h),0,Vector2(1,1),0)
+	#
 
 func _ready() -> void:
 	global_position = Vector2(0,0)
-	
+
+func detector_enter() -> void:
+	pass
+
+
+func detector_exited() -> void:
+	for h in hight:
+		for w in width:
+			tile.set_cell(0,Vector2i(w,player.position.y+h),0,Vector2(1,1),0)
