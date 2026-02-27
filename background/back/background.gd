68dd45ff9942bb64b1522b07d4fc1ab73ee862e4
F1ejm <karolosrodka@gmail.com>
2026-02-27 22:43:39 +0100

    chunk gen basic

--- background/back/background.gd
+++ background/back/background.gd
@@ -4,13 +4,20 @@ extends TileMap
 @export var tile: TileMap
 @export var player : CharacterBody2D
 
+@export var hight:=  10
+@export var width:= 10
 
 func _ready() -> void:
 	global_position = Vector2(0,0)
-	
-	tile.set_cell(0,Vector2i(10,1),0)
-
 
 
 func detector_entered() -> void:
 	tile.set_cell(0,Vector2i(0,0),0,Vector2(1,1),0)
+	
+	for h in hight:
+		for w in width:
+			tile.set_cell(0,Vector2i(w,h),0,Vector2(1,1),0)
+	
+	detector.queue_free()
+			
+			 
