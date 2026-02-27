26e283b37c08ddd0c19c9fc395f72aef7ed47d3f
F1ejm <karolosrodka@gmail.com>
2026-02-28 00:43:29 +0100

    background w osi y

--- background/back/background.gd
+++ background/back/background.gd
@@ -2,27 +2,22 @@ extends TileMap
 
 @export var tile: TileMap
 @export var player : CharacterBody2D
+@export var marker : Marker2D
 
-@export var hight:=  10
+@export var hight:=  60
 @export var width:= 30
 
-var _in = true
 
 func _process(delta: float) -> void:
-	pass
-	#for h in hight:
-		#for w in width:
-			#tile.set_cell(0,Vector2i(w,player.position.y+h),0,Vector2(1,1),0)
+	#if player.position.y == marker.position.y:
+		#print("fd")
+	for h in hight:
+		for w in width:
+			tile.set_cell(0,Vector2i(w,player.position.y+h+50),0,Vector2(1,1),0)
 	#
 
 func _ready() -> void:
 	global_position = Vector2(0,0)
-
-func detector_enter() -> void:
-	pass
-
-
-func detector_exited() -> void:
 	for h in hight:
 		for w in width:
-			tile.set_cell(0,Vector2i(w,player.position.y+h),0,Vector2(1,1),0)
+			tile.set_cell(0,Vector2i(w,h),0,Vector2(1,1),0)
