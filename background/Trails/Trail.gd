e36fa3a05ae53665ec2c877a8acac8541bc3ea66
F1ejm <karolosrodka@gmail.com>
2026-02-28 08:00:05 +0100

    zajebiste kółka

--- background/Trails/Trail.gd
+++ background/Trails/Trail.gd
@@ -11,6 +11,16 @@ var width:= 90
 
 var cave :=1
 
+@export var radius: int = 20
+@export var noise_strength: float = 1.2
+@export var tile_id: int = 0
+@export var atlas_coords: Vector2i = Vector2i(0, 0)
+
+func _ready():
+	randomize()
+	draw_rough_circle(Vector2i(0, 0), radius)
+	global_position = Vector2(0,0)
+
 func _process(delta: float) -> void:
 	
 	#for h in hight:
@@ -38,32 +48,50 @@ func _process(delta: float) -> void:
 		#pass
 #erase_cell()
 
-func _ready() -> void:
-	global_position = Vector2(0,0)
-	#for h in hight:
-		#for w in width:
-			#tile.set_cell(0,Vector2i(w,h),3,Vector2(randi_range(0,7),randi_range(2,7)),0)
 
 
 func timeout() -> void:
+	
 	var x = randi_range(-1,1)*(randi_range(100,300))
 	var y = (randi_range(300,800))
 	var pos = tile.local_to_map(Vector2(player.position)+Vector2(x,y))
-	print(player.position)
-	print(pos)
-	
-	var r = randi_range(10,20)
-	var t = randi_range(10,20)
-	
-	for u in range(3,r):
-		for h in range(3,t):
-			tile.set_cell(0,pos+Vector2i(u,h),3,Vector2i(2,1),0)
+	draw_rough_circle(pos,9)
+	pos += Vector2i(10,6)
+	draw_rough_circle(pos,7)
+	#print(player.position)
+	#print(pos)
+	#
+	#var r = randi_range(20,40)
+	#var t = randi_range(20,30)
+	#
+	#for u in range(3,r):
+		#for h in range(3,t):
+			#tile.set_cell(0,pos+Vector2i(u,h),3,Vector2i(2,1),0)
+			#
+	#var left = tile.get_neighbor_cell(pos,TileSet.CELL_NEIGHBOR_LEFT_SIDE)
+	#var down = tile.get_neighbor_cell(pos,TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)
+	#var top = tile.get_neighbor_cell(pos,TileSet.CELL_NEIGHBOR_TOP_SIDE)
+	#
+	#for b in range(4,randi_range(10,20)):
+		#tile.set_cell(0,left+Vector2i(3,b),3,Vector2i(2,1),0)
+		#tile.set_cell(0,left+Vector2i(2,b-1),3,Vector2i(2,1),0)
+		#tile.set_cell(0,left+Vector2i(1,b-2),3,Vector2i(2,1),0)
+		#
+		#tile.set_cell(0,top+Vector2i(b,3),3,Vector2i(2,1),0)
+		#tile.set_cell(0,top+Vector2i(b-1,2),3,Vector2i(2,1),0)
+		#tile.set_cell(0,top+Vector2i(b-2,1),3,Vector2i(2,1),0)
+		
+		
+
+func draw_rough_circle(center: Vector2i, r: int):
+	for x in range(-r - 5, r + 5):
+		for y in range(-r - 5, r + 5):
 			
-	var left = tile.get_neighbor_cell(pos,TileSet.CELL_NEIGHBOR_LEFT_SIDE)
-	var down = tile.get_neighbor_cell(pos,TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)
-	var top = tile.get_neighbor_cell(pos,TileSet.CELL_NEIGHBOR_TOP_SIDE)
-	
-	for b in range(3,randi_range(6,15)):
-		
-		
+			var dist = Vector2(x, y).length()
+			
+			# dodajemy losowe "poszarpanie"
+			var rough_offset = randf_range(-noise_strength, noise_strength)
+			
+			if dist <= r + rough_offset:
+				tile.set_cell(0, center + Vector2i(x, y), 3, Vector2(2,1))
 	
