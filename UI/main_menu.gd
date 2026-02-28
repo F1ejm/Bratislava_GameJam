ce63d2598e7e3f9a36562351ca04f8b7793eb56c
Nikita Dyrcz <nikitadyrcz8@gmail.com>
2026-02-28 23:54:18 +0100

    save

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,14 +1,20 @@
 extends Control
 
 var tween: Tween
-@onready var play: TextureButton = $VBoxContainer/Play
-@onready var options_help: TextureButton = $VBoxContainer/OptionsHelp
+@onready var play: TextureButton = $Play
+@onready var options_help: TextureButton = $OptionsHelp
+@onready var camera_2d: Camera2D = $Camera2D
+
 var play_initial_pos
 var optionshelp_initial_pos
+var cam_initial_pos
+
+@export var player_cam: Camera2D
 
 func _ready() -> void:
 	play_initial_pos = play.position
 	optionshelp_initial_pos = options_help.position
+	cam_initial_pos = camera_2d.position
 
 func reset_tween():
 	if tween:
@@ -19,3 +25,13 @@ func _on_play_pressed() -> void:
 	reset_tween()
 	tween.set_parallel(false)
 	tween.tween_property(play, "position", Vector2(-500, play_initial_pos.y), get_process_delta_time() * 15)
+	tween.tween_property(options_help, "position", Vector2(-500, optionshelp_initial_pos.y), get_process_delta_time() * 15)
+
+
+func _on_options_help_pressed() -> void:
+	reset_tween()
+	tween.tween_property(camera_2d, "position", Vector2(-576, 324), get_process_delta_time() * 30)
+
+func back():
+	reset_tween()
+	tween.tween_property(camera_2d, "position", cam_initial_pos, get_process_delta_time() * 30)
--- UI/settings.gd
+++ UI/settings.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var music: HSlider = $VBoxContainer/HSlider2
 @onready var sfx: HSlider = $VBoxContainer/HSlider3
 
+@export var main_menu: Control
+
 func _ready() -> void:
 	master.value = Global.master_volume
 	music.value = Global.music_volume
@@ -20,3 +22,6 @@ func _on_h_slider_2_value_changed(value: float) -> void:
 func _on_h_slider_3_value_changed(value: float) -> void:
 	Global.sfx_volume = value
 	AudioServer.set_bus_volume_linear(2, value / 100)
+
+func _on_texture_button_pressed() -> void:
+	main_menu.back()
