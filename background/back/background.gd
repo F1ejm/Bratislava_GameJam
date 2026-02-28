b5a72cd39594b1eca1968b3fa174a6d5aa35a57f
F1ejm <karolosrodka@gmail.com>
2026-02-28 13:33:33 +0100

    lsdfj

--- background/back/background.gd
+++ background/back/background.gd
@@ -4,16 +4,16 @@ extends TileMap
 @export var player : CharacterBody2D
 
 @export var hight:=  10
-@export var width:= 90
+@export var width:= 80
 
 func _process(delta: float) -> void:
 	for h in hight:
 		for w in width:
 			tile.set_cell(0,Vector2i(w,player.position.y+h+50),0,Vector2(randi_range(0,7),randi_range(2,7)),0)
-
 func _ready() -> void:
 	global_position = Vector2(0,0)
 	player.position.x = 500
 	for h in 100:
 		for w in width:
+			
 			tile.set_cell(0,Vector2i(w,h),0,Vector2(randi_range(0,7),randi_range(2,7)),0)
