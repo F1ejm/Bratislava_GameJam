a8bcccacd1f596119f6c2d93b7c9619d5177d8ce
KamilZago <kamil.zagorny1802@gmail.com>
2026-02-28 22:19:51 +0100

    Update ore.gd

--- Main/ore.gd
+++ Main/ore.gd
@@ -58,5 +58,5 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("Player")):
-		Global.score += worth
+		Global.score += worth * (1+ body.upgreawards * 0.1)
 		queue_free()
