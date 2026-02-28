9d30055108db8bf13c10c5cf8839f610fe2cd346
Nikita Dyrcz <nikitadyrcz8@gmail.com>
2026-02-28 22:42:43 +0100

    ui szpont

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,17 +1,14 @@
 extends Control
 
 var tween: Tween
-@onready var play: TextureButton = $Play
-@onready var options: TextureButton = $Options
-@onready var quit: TextureButton = $Quit
+@onready var play: TextureButton = $VBoxContainer/Play
+@onready var options_help: TextureButton = $VBoxContainer/OptionsHelp
 var play_initial_pos
-var options_initial_pos
-var quit_initial_pos
+var optionshelp_initial_pos
 
 func _ready() -> void:
 	play_initial_pos = play.position
-	options_initial_pos = options.position
-	quit_initial_pos = quit.position
+	optionshelp_initial_pos = options_help.position
 
 func reset_tween():
 	if tween:
@@ -22,17 +19,3 @@ func _on_play_pressed() -> void:
 	reset_tween()
 	tween.set_parallel(false)
 	tween.tween_property(play, "position", Vector2(-500, play_initial_pos.y), get_process_delta_time() * 15)
-	tween.tween_property(options, "position", Vector2(-500, options_initial_pos.y), get_process_delta_time() * 15)
-	tween.tween_property(quit, "position", Vector2(-500, quit_initial_pos.y), get_process_delta_time() * 15)
-
-
-func _on_options_pressed() -> void:
-	reset_tween()
-	tween.set_parallel(false)
-	tween.tween_property(play, "position", Vector2(-500, play_initial_pos.y), get_process_delta_time() * 15)
-	tween.tween_property(options, "position", Vector2(-500, options_initial_pos.y), get_process_delta_time() * 15)
-	tween.tween_property(quit, "position", Vector2(-500, quit_initial_pos.y), get_process_delta_time() * 15)
-
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
