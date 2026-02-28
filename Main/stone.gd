8df47f66720342210a56a8adb26c41436e33dbf2
KamilZago <kamil.zagorny1802@gmail.com>
2026-02-28 14:14:54 +0100

    kamienie

---
+++ Main/stone.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	match randi_range(0,2):
+		0:
+			$StaticBody2D/CollisionShape2D2.queue_free()
+			$StaticBody2D/CollisionShape2D3.queue_free()
+		1:
+			$StaticBody2D/CollisionShape2D.queue_free()
+			$StaticBody2D/CollisionShape2D3.queue_free()
+		2:
+			$StaticBody2D/CollisionShape2D.queue_free()
+			$StaticBody2D/CollisionShape2D2.queue_free()
+	var sca = randf_range(2,25)
+	scale = Vector2(sca,sca)
+	rotation = randf_range(0,2*PI)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
