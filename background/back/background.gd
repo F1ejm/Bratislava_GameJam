25bf8ca9fbbbe4fd9f288329fab76c478003bdbb
F1ejm <karolosrodka@gmail.com>
2026-02-28 13:55:21 +0100

    lskjdf

--- background/back/background.gd
+++ background/back/background.gd
@@ -10,6 +10,10 @@ func _process(delta: float) -> void:
 	for h in hight:
 		for w in width:
 			tile.set_cell(0,Vector2i(w,player.position.y+h+50),0,Vector2(randi_range(0,7),randi_range(2,7)),0)
+	for h in hight:
+		tile.set_cell(0,Vector2i(-1,player.position.y + h),4,Vector2i(3,randi_range(0,7)),0)
+
+
 func _ready() -> void:
 	global_position = Vector2(0,0)
 	player.position.x = 500
