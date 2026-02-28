d77c2c609e55592e23f7823bf93575255e741b96
F1ejm <karolosrodka@gmail.com>
2026-02-28 02:20:45 +0100

    dig it up

--- background/Trails/Trail.gd
+++ background/Trails/Trail.gd
@@ -6,19 +6,33 @@ var mouse: Vector2i
 @export var player : CharacterBody2D
 
 var hight:=  60
-var width:= 30
+var width:= 60
 
 func _process(delta: float) -> void:
 	for h in hight:
 		for w in width:
 			tile.set_cell(0,Vector2i(w,player.position.y+h+50),2,Vector2(0,0),0)
 	
+
 	
 	
 	var clicked_cell = tile.local_to_map(player.position)
+	
+	var tile_p = tile.get_neighbor_cell(clicked_cell,TileSet.CELL_NEIGHBOR_RIGHT_SIDE)
+	var tile_l = tile.get_neighbor_cell(clicked_cell,TileSet.CELL_NEIGHBOR_LEFT_SIDE)
+	var tile_d = tile.get_neighbor_cell(clicked_cell,TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)
 	#print(clicked_cell)
+	
+	var tile_ll = tile.get_neighbor_cell(tile_l,TileSet.CELL_NEIGHBOR_TOP_LEFT_CORNER)
+	
 	tile.erase_cell(0,clicked_cell)
+	tile.erase_cell(0,tile_d)
+	tile.erase_cell(0,tile_l)
+	tile.erase_cell(0,tile_p)
 	var data = tile.get_cell_tile_data(0, clicked_cell)
+	
+	
+	
 	
 	
 	#if data:
