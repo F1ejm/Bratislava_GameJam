7254ae0d6ec16c873029da1d78cc0a2bbf9e5d31
F1ejm <karolosrodka@gmail.com>
2026-02-28 18:38:20 +0100

    szparka beatki

--- background/back/background.gd
+++ background/back/background.gd
@@ -9,13 +9,12 @@ extends TileMap
 var i = false
 
 func _process(delta: float) -> void:
-	if i != false:
-		for h in hight:
-			for w in width:
-				tile.set_cell(0,Vector2i(w,player.position.y+h+50),0,Vector2(randi_range(0,7),randi_range(2,7)),0)
-		for h in hight:
-			tile.set_cell(0,Vector2i(-1,player.position.y + h),4,Vector2i(3,randi_range(0,7)),0)
-			tile.set_cell(0,Vector2i(80,player.position.y + h),3,Vector2i(0,randi_range(0,7)),0)
+	for h in hight:
+		for w in width:
+			tile.set_cell(0,Vector2i(w,player.position.y+h+50),0,Vector2(randi_range(0,7),randi_range(2,7)),0)
+	for h in hight:
+		tile.set_cell(0,Vector2i(-1,player.position.y + h),4,Vector2i(3,randi_range(0,7)),0)
+		tile.set_cell(0,Vector2i(80,player.position.y + h),3,Vector2i(0,randi_range(0,7)),0)
 	
 
 
@@ -29,7 +28,3 @@ func _ready() -> void:
 		tile.set_cell(0,Vector2i(-1, h),4,Vector2i(3,randi_range(0,7)),0)
 		tile.set_cell(0,Vector2i(80, h),3,Vector2i(0,randi_range(0,7)),0)
 	
-
-
-func _on_timer_timeout() -> void:
-	i = true
