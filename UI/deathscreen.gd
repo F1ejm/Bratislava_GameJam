f12c68011a1ee12d63242f589af6eed78ffaea6d
F1ejm <karolosrodka@gmail.com>
2026-03-01 03:52:20 +0100

    fjs

--- UI/deathscreen.gd
+++ UI/deathscreen.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var label : Label
+@onready var back = preload("res://Main/Main.tscn").instantiate()
 
 func _process(delta: float) -> void:
 	label.text  = str(Global.score)
@@ -8,5 +9,4 @@ func _process(delta: float) -> void:
 
 
 func _on_button_button_down() -> void:
-	var back = preload("res://Main/Main.tscn").instantiate()
-	get_tree().root.add_child(back)
+	get_tree().change_scene_to_file("res://Main/Main.tscn")
