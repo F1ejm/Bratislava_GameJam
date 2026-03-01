10358f4697764f6ff9e643c41b99e8adf656c76a
F1ejm <karolosrodka@gmail.com>
2026-03-01 04:03:19 +0100

    final 1

--- Miner/hp.gd
+++ Miner/hp.gd
@@ -4,6 +4,12 @@ extends Control
 @export var rect2 : TextureRect
 @export var rect3 : TextureRect
 
+func _ready() -> void:
+	rect1.texture = preload("res://grafiki/żarówka.png")
+	rect2.texture = preload("res://grafiki/żarówka.png")
+	rect3.texture = preload("res://grafiki/żarówka.png")
+	Global.hp = 3
+
 func _process(delta: float) -> void:
 	if Global.game_paused == true:
 		hide()
@@ -22,6 +28,7 @@ func _process(delta: float) -> void:
 		rect2.texture = preload("res://grafiki/żarówka2.png")
 		rect3.texture = preload("res://grafiki/żarówka.png")
 	elif Global.hp == 0:
-		rect1.texture = preload("res://grafiki/Sprite-0001.png")
-		rect2.texture = preload("res://grafiki/Sprite-0001.png")
-		rect3.texture = preload("res://grafiki/Sprite-0001.png")
+		rect1.texture = preload("res://grafiki/żarówka2.png")
+		rect2.texture = preload("res://grafiki/żarówka2.png")
+		rect3.texture = preload("res://grafiki/żarówka2.png")
+		
--- UI/deathscreen.gd
+++ UI/deathscreen.gd
@@ -4,9 +4,11 @@ extends Control
 @onready var back = preload("res://Main/Main.tscn").instantiate()
 
 func _process(delta: float) -> void:
-	label.text  = str(Global.score)
+	label.text  = str(int(Global.score))
 	
 
 
 func _on_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://Main/Main.tscn")
+	Global.game_paused = true
+	Global.score = 0
